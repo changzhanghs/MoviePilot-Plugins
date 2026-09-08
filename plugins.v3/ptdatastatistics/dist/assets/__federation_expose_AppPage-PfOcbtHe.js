28d21f25a21084f039e21203c8ef20116fe1eec8
cz <changzhanghs@gmail.com>
2026-09-08 11:12:38 +0800

    fix(ptdatastatistics): release v1.0.1

--- plugins.v3/ptdatastatistics/dist/assets/__federation_expose_AppPage-PfOcbtHe.js
+++ plugins.v3/ptdatastatistics/dist/assets/__federation_expose_AppPage-PfOcbtHe.js
@@ -1,5 +1,5 @@
 import { importShared } from './__federation_fn_import-JrT3xvdd.js';
-import { P as PTStatsWorkbench } from './PTStatsWorkbench-Untad9Cn.js';
+import { P as PTStatsWorkbench } from './PTStatsWorkbench-CZSFnjfl.js';
 
 const {openBlock:_openBlock,createBlock:_createBlock} = await importShared('vue');
 
